5f95f48ad09a5efa3c6d23223251404d151553ea
hung nguyen van <nvh66725@gmail.com>
2021-05-29 22:43:19 +0700

    todo list angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { v4 } from 'uuid';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo-angular';
+
+  listTask: Task[] = [
+    { id: v4(), name: 'sleep', done: false },
+    { id: v4(), name: 'breakfast', done: true },
+    { id: v4(), name: 'uong ruou', done: false },
+    { id: v4(), name: 'nau com', done: true },
+    { id: v4(), name: 'nau com', done: true },
+  ];
 }
